775720e24722d5171f7a7c0d5888651a22f9c9ed
Rani Budhi <ranibudhi257@gmail.com>
2026-07-18 16:38:55 +0700

    Revert "Merge pull request #2 from larasatirida/feature/auth-input-jadwal"
    This reverts commit 016f4cf6cac62665130f12120d30ce3c47a194f1, reversing
    changes made to bd92e51f94f7a70e7e09af8ce138fa4201b4c3f7.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,14 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.google.gms.google.services)
-    alias(libs.plugins.kotlin.android)
 }
 
 android {
     namespace = "com.example.warasapp"
-    compileSdk = 36
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
 
     defaultConfig {
         applicationId = "com.example.warasapp"
@@ -14,33 +16,28 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            optimization {
+                enable = false
+            }
         }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
-    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
     implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
-    implementation(libs.firebase.firestore)
-    implementation("com.google.firebase:firebase-auth-ktx")
-    implementation("com.google.firebase:firebase-analytics")
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
